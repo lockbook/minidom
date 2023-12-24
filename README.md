255065eae03f779732b82e0ab30ccf3ca60d10af
ad-tra <aew31gcmuy@gmail.com>
2023-12-24 03:03:10 -0500

    Revert "delete by id instead of namespace"
    This reverts commit 76ac4ae341ef844dab0a08dba163efb9f06919a9.

--- README.md
+++
@@ -1,8 +0,0 @@
-minidom
-=======
-
-What's this?
-------------
-
-A minimal DOM library on top of rxml, targeting exclusively the subset of
-XML useful for XMPP.
--- target/debug/build/smartstring-dc26cdfe1a284eb1/build_script_build-dc26cdfe1a284eb1.d
+++
@@ -1,5 +0,0 @@
-/home/adam/Documents/programation-projects/work/lockbook-inc/minidom/target/debug/build/smartstring-dc26cdfe1a284eb1/build_script_build-dc26cdfe1a284eb1: /home/adam/.cargo/registry/src/index.crates.io-6f17d22bba15001f/smartstring-1.0.1/./build.rs
-
-/home/adam/Documents/programation-projects/work/lockbook-inc/minidom/target/debug/build/smartstring-dc26cdfe1a284eb1/build_script_build-dc26cdfe1a284eb1.d: /home/adam/.cargo/registry/src/index.crates.io-6f17d22bba15001f/smartstring-1.0.1/./build.rs
-
-/home/adam/.cargo/registry/src/index.crates.io-6f17d22bba15001f/smartstring-1.0.1/./build.rs:
