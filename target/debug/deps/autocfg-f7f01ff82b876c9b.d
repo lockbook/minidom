101e20650f435f9601ecff7ee70fcdf8c95e6892
ad-tra <aew31gcmuy@gmail.com>
2023-12-24 03:04:48 -0500

    gitignore

--- target/debug/deps/autocfg-f7f01ff82b876c9b.d
+++
@@ -1,9 +0,0 @@
-/home/adam/Documents/programation-projects/work/lockbook-inc/minidom/target/debug/deps/autocfg-f7f01ff82b876c9b.rmeta: /home/adam/.cargo/registry/src/index.crates.io-6f17d22bba15001f/autocfg-1.1.0/src/lib.rs /home/adam/.cargo/registry/src/index.crates.io-6f17d22bba15001f/autocfg-1.1.0/src/error.rs /home/adam/.cargo/registry/src/index.crates.io-6f17d22bba15001f/autocfg-1.1.0/src/version.rs
-
-/home/adam/Documents/programation-projects/work/lockbook-inc/minidom/target/debug/deps/libautocfg-f7f01ff82b876c9b.rlib: /home/adam/.cargo/registry/src/index.crates.io-6f17d22bba15001f/autocfg-1.1.0/src/lib.rs /home/adam/.cargo/registry/src/index.crates.io-6f17d22bba15001f/autocfg-1.1.0/src/error.rs /home/adam/.cargo/registry/src/index.crates.io-6f17d22bba15001f/autocfg-1.1.0/src/version.rs
-
-/home/adam/Documents/programation-projects/work/lockbook-inc/minidom/target/debug/deps/autocfg-f7f01ff82b876c9b.d: /home/adam/.cargo/registry/src/index.crates.io-6f17d22bba15001f/autocfg-1.1.0/src/lib.rs /home/adam/.cargo/registry/src/index.crates.io-6f17d22bba15001f/autocfg-1.1.0/src/error.rs /home/adam/.cargo/registry/src/index.crates.io-6f17d22bba15001f/autocfg-1.1.0/src/version.rs
-
-/home/adam/.cargo/registry/src/index.crates.io-6f17d22bba15001f/autocfg-1.1.0/src/lib.rs:
-/home/adam/.cargo/registry/src/index.crates.io-6f17d22bba15001f/autocfg-1.1.0/src/error.rs:
-/home/adam/.cargo/registry/src/index.crates.io-6f17d22bba15001f/autocfg-1.1.0/src/version.rs:
--- target/debug/examples/articles-82d8d9e0901e781f.d
+++
@@ -1,5 +0,0 @@
-/home/adam/Documents/programation-projects/work/lockbook-inc/minidom/target/debug/examples/articles-82d8d9e0901e781f.rmeta: examples/articles.rs
-
-/home/adam/Documents/programation-projects/work/lockbook-inc/minidom/target/debug/examples/articles-82d8d9e0901e781f.d: examples/articles.rs
-
-examples/articles.rs:
